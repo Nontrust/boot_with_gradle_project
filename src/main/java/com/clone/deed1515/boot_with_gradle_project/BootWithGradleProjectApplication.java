43d9661f2b98298e57153f58531c7ce4140dd264
소프트웍스 이충호 <deed1515@softworks.co.kr>
2022-06-22 11:22:51 +0900

    1. Initial commit -start with spring.start.io

---
+++ src/main/java/com/clone/deed1515/boot_with_gradle_project/BootWithGradleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.clone.deed1515.boot_with_gradle_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootWithGradleProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootWithGradleProjectApplication.class, args);
+    }
+
+}
